952a96ad6a78079f92687e1f77f23e4e5492c984
Lígia Yumi <ligiayumimeira@gmail.com>
2025-11-26 13:50:24 -0300

    Implement comments
    Added comments.

--- atividades/calculadora.c
+++ atividades/calculadora.c
@@ -1,18 +1,22 @@
 //1.calculadora modular com funções
 #include <stdio.h>
 
+//função de soma
 void soma(int n[2]){
   printf("\n%d + %d = %d\n", n[0], n[1], n[0]+n[1]);
 }
 
+//função subtração
 void subtracao(int n[2]){
   printf("\n%d - %d = %d\n", n[0], n[1], n[0]-n[1]);
 }
 
+//função multiplicação
 void multiplicacao(int n[2]){
   printf("\n%d x %d = %d\n", n[0], n[1], n[0]*n[1]);
 }
 
+//função divisão
 void divisao(int n[2]){
   if(n[1] != 0){
       printf("\n%d : %d = %d\n", n[0], n[1], n[0]/n[1]);
@@ -21,6 +25,7 @@ void divisao(int n[2]){
   }
 }
 
+//função resto da divisão
 void restoDiv(int n[2]){
   printf("\nO resto da divisão %d : %d é %d\n", n[0], n[1], n[0]%n[1]);
 }
@@ -28,11 +33,13 @@ void restoDiv(int n[2]){
 int main() {
   int n[2], opcao;
 
+  //laço para ler os númeroa
   printf("Digite dois números:\n");
   for(int i = 0; i < 2; i++){
     scanf("%d", &n[i]);
   }
 
+  //menu para usuário escolher quais operações quer realizar
   do{
     printf("\n----MENU----\n1.Soma\n2.Subtração\n3.Multiplicação\n4.Divisão\n5.Resto de divisão\n6.Sair\n");
     printf("Escolha o que deseja fazer: ");
